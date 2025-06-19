084dfa072d7f9e567943510d524691fe19feb9b2
lance hester <lhestergs4@gmail.com>
2025-06-19 13:16:56 -0400

    fixing whitespaces on build_vector_store.py

--- src/app.py
+++ src/app.py
@@ -1,46 +1,36 @@
-from pathlib import Path
+from build_vector_store import FAISS_INDEX, MODEL
 
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 
-from langchain_ollama import ChatOllama
 from langchain_ollama.llms import OllamaLLM
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-import ollama
 from operator import itemgetter
 import streamlit as st
 
 
 # Make sure that the Ollama server is running.
 
-# Setting the Constants
-data_directory_path = Path(__file__).resolve().parent.parent /"data"
-IPOD_PDF = f"{data_directory_path.joinpath("ipod_shuffle_2015_user_guide.pdf")}"
-MODEL = "llama3.2"
-
-# Loading the PDF data source for the RAG model
-loader = PyPDFLoader(IPOD_PDF)
-pages = loader.load()
-
-# Need to Split PDF into Chunks for ease of tokenization and Semanitc Search
-splitter = RecursiveCharacterTextSplitter(chunk_size=1500,chunk_overlap=100)
-chunks = splitter.split_documents(pages)
-
-# Need to pull the llam3.1 model
-ollama.pull(MODEL)
+# Need to pull the llam3.2 model if I don't already have it.
+# I have the model so the line of code below is commented out.
+#ollama.pull(MODEL) 
 
 # Create the vector database
 ollama_endpoint = "http://127.0.0.1:11434"
 embeddings = OllamaEmbeddings(model=MODEL, base_url=ollama_endpoint)
-vectorstore = FAISS.from_documents(chunks, embeddings)
+
+#Get stored Vector Store
+vector_store = FAISS.load_local(
+    FAISS_INDEX, 
+    embeddings, 
+    allow_dangerous_deserialization=True
+)
 
 # Setup a Receiver
-retriever = vectorstore.as_retriever()
+retriever = vector_store.as_retriever()
 
 # Creating the model
 model = OllamaLLM(model=MODEL)
